74e079270dcf69f55900da5a2018f37b35e52f07
Cetley7 <cheboiwendy@gmail.com>
2023-06-15 14:03:50 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(message);
-    ssize_t ret;
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = strlen(message);
+	ssize_t ret;
 
-    ret = write(STDERR_FILENO, message, len);
-    if (ret == -1)
-    {
-        /* Error occurred while writing */
-        const char *error_message = strerror(errno);
-        ssize_t error_len = strlen(error_message);
-        write(STDERR_FILENO, error_message, error_len);
-        return 1;
-    }
+	ret = write(STDERR_FILENO, message, len);
+	if (ret == -1)
+	{
+		/* Error occurred while writing */
+		const char *error_message = strerror(errno);
+		ssize_t error_len = strlen(error_message);
+		write(STDERR_FILENO, error_message, error_len);
+		return (1);
+	}
 
-    return 1;
+	return (1);
 }
