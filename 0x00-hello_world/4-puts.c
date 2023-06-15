ecec7006f620653ee03b859cdbf0a5b739d8454f
Cetley7 <cheboiwendy@gmail.com>
2023-06-15 13:25:43 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function.
- *
- * Return: 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-}}
+}
