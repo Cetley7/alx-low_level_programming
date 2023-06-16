993fa43f8fb16032a10c39585f9100b9d03ff052
Cetley7 <cheboiwendy@gmail.com>
2023-06-16 15:02:50 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,23 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			for (num3 = num1; num3 <= 9; num3++)
+			for (num3 = 0; num3 <= 9; num3++)
 			{
-				for (num4 = num2 + 1; num4 <= 9; num4++)
+				for (num4 = 0; num4 <= 9; num4++)
 				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
-
-					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+					if ((num1 * 10 + num2) < (num3 * 10 + num4))
 					{
-						putchar(',');
+						putchar(num1 + '0');
+						putchar(num2 + '0');
 						putchar(' ');
+						putchar(num3 + '0');
+						putchar(num4 + '0');
+
+						if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
