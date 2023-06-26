c23cd0f58ccbcb0f4d5b3529abe8cf354cac8990
Cetley7 <cheboiwendy@gmail.com>
2023-06-26 12:05:12 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to the string to be reversed
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+		length++;
+
+	end = length - 1;
+
+	/* Swap characters from start and end indices until they meet in the middle */
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
