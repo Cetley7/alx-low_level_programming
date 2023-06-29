df45448853bd4b85d9668851a71ae60e8e714a85
Cetley7 <cheboiwendy@gmail.com>
2023-06-29 16:40:12 -0700

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,28 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    /* Capitalize first letter */
-    if (str[i] >= 'a' && str[i] <= 'z')
-        str[i] -= 32;
+	/* Capitalize first letter */
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
 
-    while (str[i] != '\0')
-    {
-        /* Check for separator characters */
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
-            /* Capitalize next letter */
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] -= 32;
-        }
+	while (str[i] != '\0')
+	{
+		/* Check for separator characters */
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}')
+		{
+			/* Capitalize next letter */
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
 
-        i++;
-    }
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
