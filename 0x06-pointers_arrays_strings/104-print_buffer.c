d0d2b508bb0366a1af789da8779fdfe2c7f6f333
Cetley7 <cheboiwendy@gmail.com>
2023-06-30 07:25:02 -0700

    commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,48 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
- * print_buffer - Prints the content of a buffer.
- * @b: The buffer to print.
- * @size: The number of bytes to print.
+ * print_buffer - Prints the content of a buffer
+ * @b: The buffer to print
+ * @size: The size of the buffer
  */
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int a, c;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	for (a = 0; a < size; a += 10)
+	{
+		printf("%08x: ", a);
 
-        /* Print hexadecimal content */
-        for (j = i; j < i + 10; j++)
-        {
-            if (j < size)
-                printf("%02x", (unsigned char)b[j]);
-            else
-                printf("  ");
+		for (c = 0; c < 10; c++)
+		{
+			if (a + c < size)
+				printf("%02x", b[a + c]);
+			else
+				printf("  ");
 
-            if (j % 2 == 1)
-                printf(" ");
-        }
+			if (c % 2 != 0)
+				printf(" ");
+		}
 
-        printf(" ");
+		for (c = 0; c < 10; c++)
+		{
+			if (a + c >= size)
+				break;
 
-        /* Print ASCII representation */
-        for (j = i; j < i + 10; j++)
-        {
-            if (j >= size)
-                printf(" ");
-            else if (b[j] >= 32 && b[j] <= 126)
-                printf("%c", b[j]);
-            else
-                printf(".");
-        }
+			if (isprint(b[a + c]))
+				printf("%c", b[a + c]);
+			else
+				printf(".");
+		}
 
-        printf("\n");
-    }
+		printf("\n");
+	}
 }
