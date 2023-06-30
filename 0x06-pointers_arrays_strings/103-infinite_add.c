7d539bf64e83945c8060019adccbcff0972bd9b0
Cetley7 <cheboiwendy@gmail.com>
2023-06-30 07:39:17 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,57 +1,71 @@
 #include "main.h"
 
 /**
+ * reverse_string - Reverses a string.
+ * @str: The string to reverse.
+ */
+void reverse_string(char *str)
+{
+	int len = 0;
+	int i, j;
+	char temp;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
+}
+
+/**
  * infinite_add - Adds two numbers.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
+ * @n1: The first number.
+ * @n2: The second number.
  * @r: The buffer to store the result.
  * @size_r: The size of the buffer.
  *
- * Return: A pointer to the result.
+ * Return: Pointer to the result (r) or 0 if the result cannot be stored in r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
-	int i, j;
+	int len1 = 0, len2 = 0;
+	int i, j, carry = 0;
 
-	/* Calculate the lengths of the input strings */
 	while (n1[len1] != '\0')
 		len1++;
+
 	while (n2[len2] != '\0')
 		len2++;
 
-	if (len1 > size_r || len2 > size_r)
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
 		return (0);
 
-	/* Add digits from right to left */
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
-	{
-		sum = carry;
+	i = len1 - 1;
+	j = len2 - 1;
+	int k = 0;
 
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		int sum = carry;
 		if (i >= 0)
 			sum += n1[i] - '0';
 		if (j >= 0)
 			sum += n2[j] - '0';
 
+		r[k++] = sum % 10 + '0';
 		carry = sum / 10;
-		sum %= 10;
 
-		/* Store the result in reverse order */
-		r[size_r - 1] = sum + '0';
-		size_r--;
+		i--;
+		j--;
 	}
 
-	if (size_r == 0 && carry)
-		return (0);
+	r[k] = '\0';
 
-	/* Shift the result to the right if necessary */
-	if (size_r > 0 && carry)
-	{
-		for (i = size_r - 1; i >= 0; i--)
-			r[i + 1] = r[i];
+	reverse_string(r);
 
-		r[0] = carry + '0';
-	}
-
-	return (&r[size_r]);
+	return (r);
 }
