705e5ec4745f5e65a5345c2cf08659197f5618d6
Cetley7 <cheboiwendy@gmail.com>
2023-06-30 07:49:33 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,71 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * reverse_string - Reverses a string.
- * @str: The string to reverse.
- */
-void reverse_string(char *str)
-{
-	int len = 0;
-	int i, j;
-	char temp;
-
-	while (str[len] != '\0')
-		len++;
-
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-	}
-}
-
-/**
  * infinite_add - Adds two numbers.
- * @n1: The first number.
- * @n2: The second number.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
  * @r: The buffer to store the result.
  * @size_r: The size of the buffer.
  *
- * Return: Pointer to the result (r) or 0 if the result cannot be stored in r.
+ * Return: A pointer to the result or 0 if the result cannot be stored in r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0;
-	int i, j, carry = 0;
+    int len1, len2, i, j, carry, sum;
 
-	while (n1[len1] != '\0')
-		len1++;
+    len1 = 0;
+    while (n1[len1] != '\0')
+        len1++;
 
-	while (n2[len2] != '\0')
-		len2++;
+    len2 = 0;
+    while (n2[len2] != '\0')
+        len2++;
 
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
-		return (0);
+    if (len1 >= size_r || len2 >= size_r)
+        return (0);
 
-	i = len1 - 1;
-	j = len2 - 1;
-	int k = 0;
+    carry = 0;
+    i = len1 - 1;
+    j = len2 - 1;
+    r[size_r - 1] = '\0';
 
-	while (i >= 0 || j >= 0 || carry > 0)
-	{
-		int sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
+    while (i >= 0 || j >= 0 || carry)
+    {
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i--] - '0';
+        if (j >= 0)
+            sum += n2[j--] - '0';
 
-		r[k++] = sum % 10 + '0';
-		carry = sum / 10;
+        carry = sum / 10;
+        r[--size_r] = (sum % 10) + '0';
+    }
 
-		i--;
-		j--;
-	}
+    if (size_r > 0 && r[0] == '\0')
+    {
+        for (i = 0; i < len1 + len2; i++)
+            r[i] = r[i + 1];
+    }
 
-	r[k] = '\0';
-
-	reverse_string(r);
-
-	return (r);
+    return (r);
 }
