556808703f5993e14452f4c6d41343a46470fe83
Cetley7 <cheboiwendy@gmail.com>
2023-07-04 12:10:02 -0700

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,6 @@
 
 /**
  * _strchr - locates a character in a string
- *
  * @s: the string to check
  * @c: the character we're looking for
  *
@@ -12,16 +11,16 @@
  */
 char *_strchr(char *s, char c)
 {
-        while (*s)
-        {
-                if (*s != c)
-                        s++;
-                else
-                        return (s);
-        }
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
 
-        if (c == '\0')
-                return (s);
+	if (c == '\0')
+		return (s);
 
-        return (NULL);
+	return (NULL);
 }
