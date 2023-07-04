cd286eb0fbbbd2917527bdb3e1bb630d6e8e7cb7
Cetley7 <cheboiwendy@gmail.com>
2023-07-04 12:48:45 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i, j;
-    unsigned int count = 0;
+	int i, j;
+	unsigned int count = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] != ' ')
-        {
-            for (j = 0; accept[j] != '\0'; j++)
-            {
-                if (s[i] == accept[j])
-                    count++;
-            }
-        }
-        else
-        {
-            return (count);
-        }
-    }
-    return (count);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != ' ')
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++;
+			}
+		}
+		else
+		{
+			return (count);
+		}
+	}
+
+	return (count);
 }
