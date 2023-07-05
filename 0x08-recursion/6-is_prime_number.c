faba30671121ce6cb71d22ff53f608ae0fc31ef7
Cetley7 <cheboiwendy@gmail.com>
2023-07-05 15:45:25 -0700

    commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_divisible - Checks if a number is divisible by any number less than it.
+ * @n: The number to check.
+ * @divisor: The current divisor to check.
+ *
+ * Return: 1 if the number is divisible by any number less than it, otherwise 0.
+ */
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1) /* Base case: divisor reaches 1, number is prime */
+		return (1);
+
+	if (n % divisor == 0) /* Base case: number is divisible by divisor, not prime */
+		return (0);
+
+	return (is_divisible(n, divisor - 1)); /* Recursively check the next divisor */
+}
+
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, otherwise 0.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1) /* Base case: numbers less than or equal to 1 are not prime */
+		return (0);
+
+	return (is_divisible(n, n - 1));
+}
