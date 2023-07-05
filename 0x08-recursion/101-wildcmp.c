044f35a3a1cbeb29123425298b940bade1c35a09
Cetley7 <cheboiwendy@gmail.com>
2023-07-05 16:44:26 -0700

    commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0') /* Base case: s2 ends with '*' */
+			return (1);
+
+		if (*s1 == '\0') /* Base case: s1 reaches the end */
+			return (0);
+
+		if (*(s2 + 1) == '*') /* Ignore consecutive '*' characters in s2 */
+			return wildcmp(s1, s2 + 1);
+
+		if (*s1 == *(s2 + 1)) /* Recursive case: check if remaining strings match */
+			return (wildcmp(s1 + 1, s2 + 2) || wildcmp(s1 + 1, s2));
+
+		return (wildcmp(s1 + 1, s2)); /* Recursive case: skip a character in s1 */
+	}
+
+	if (*s1 == *s2) /* Recursive case: current characters match */
+	{
+		if (*s1 == '\0') /* Base case: both strings reach the end */
+			return (1);
+
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0); /* Base case: characters do not match */
+}
