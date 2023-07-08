8df3af647b8fe85571cc17bbbd0d404e5a83088f
Cetley7 <cheboiwendy@gmail.com>
2023-07-07 18:03:12 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,30 +3,30 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @argc: The argument count
- * @argv: The argument vector
- *
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
  * Return: 1 if not enough arguments passed in, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int x,y;
 
 	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 	else
 	{
-		result = 1;
+		y = 1;
 
-		for (num1 = 1; num1 < 3; num1++)
-			result *= atoi(argv[num1]);
+		for (x = 1; x < 3; x++)
+			y *= atoi(argv[x]);
 
-		printf("%d\n", result);
+		printf("%d\n", y);
 	}
 
-	return 0;
+	return (0);
 }
