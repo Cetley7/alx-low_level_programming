2c2585679ced079aa04d55085d1a92e8dd0bb7dd
Cetley7 <cheboiwendy@gmail.com>
2023-07-17 14:41:57 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - Dog attributes
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- * Description: The attributes of a dog.
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a string containing the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a string containing the dog owner's name
+ *
+ * Description: This structure represents a dog and its attributes.
  */
 struct dog
 {
@@ -14,16 +15,6 @@ struct dog
     float age;
     char *owner;
 };
-
-/**
- * dog_t - Typedef for dog structure
- */
 typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /* _DOG_H_ */
+#endif /* DOG_H */
 
