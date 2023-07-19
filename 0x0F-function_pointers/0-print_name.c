b6b9213b015a438ddad8fec686bcee6c6cb43941
Cetley7 <cheboiwendy@gmail.com>
2023-07-19 11:55:47 -0700

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: Pointer to a function that will be used to print the name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+        f(name);
+}
+
