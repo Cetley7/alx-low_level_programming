649f8df613cbafea9cdf0cd1d07f97f9dd121260
Cetley7 <cheboiwendy@gmail.com>
2023-07-19 15:26:56 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-#include "function_pointers.h"
 
 /**
  * main - Simple calculator program.
@@ -12,30 +11,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	int result;
+	int num1, num2, result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL)
+	if (op_func == NULL || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
 
 	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit(100);
+		return (100);
 	}
 
 	result = op_func(num1, num2);
