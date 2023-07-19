5a4fcf8416de748effb0cd766deae2fddfec571d
Cetley7 <cheboiwendy@gmail.com>
2023-07-19 15:09:09 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,44 +5,42 @@
 
 /**
  * main - Simple calculator program.
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * @argc: The number of arguments.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int result;
-    int (*op_func)(int, int);
+	int num1, num2;
+	int result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,17 +1,21 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * op_add - Returns the sum of a and b.
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: 0
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
@@ -19,11 +23,11 @@ int op_add(int a, int b)
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: 0
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
@@ -31,11 +35,11 @@ int op_sub(int a, int b)
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: 0
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
@@ -43,11 +47,11 @@ int op_mul(int a, int b)
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: 0
+ * Return: The result of the division of a by b.
  */
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
@@ -55,10 +59,10 @@ int op_div(int a, int b)
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: 0
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
 
