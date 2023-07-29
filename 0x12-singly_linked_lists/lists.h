a5bd2266a41f06dc44ed6545790d0b116c48849e
Cetley7 <cheboiwendy@gmail.com>
2023-07-29 11:24:36 -0700

    commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /* Structure for the linked list */
-typedef struct list_s {
+typedef struct list_s
+{
     char *str;
     struct list_s *next;
 } list_t;
 
-/* Function to add a new node at the beginning of the list */
+/* Function prototypes */
 list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
 
