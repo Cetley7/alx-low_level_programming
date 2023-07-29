e4fa266da7f661e2a672c720d301eaab52de8649
Cetley7 <cheboiwendy@gmail.com>
2023-07-29 13:23:08 -0700

    committ

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,13 +13,25 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 /* Function prototype to print all elements of the list */
 size_t print_list(const list_t *h);
+
+/* Function prototype to get the number of elements in the list */
+size_t list_len(const list_t *h);
+
+/* Function prototype to add a new node at the beginning of the list */
+list_t *add_node(list_t **head, const char *str);
+
+/* Function prototype to add a new node at the end of the list */
+list_t *add_node_end(list_t **head, const char *str);
+
+/* Function prototype to free a list */
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
 
