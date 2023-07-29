af65c328a094da44385366979a73bcda0d00b7b8
Cetley7 <cheboiwendy@gmail.com>
2023-07-29 10:20:56 -0700

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the linked list.
- * @str: String to be duplicated and stored in the new node.
+ * add_node - Adds a new node at the beginning of the list.
+ * @head: Pointer to a pointer to the first node of the list.
+ * @str: The string to be duplicated and stored in the new node.
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return: The address of the new node, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+    /* Create a new node and allocate memory for it */
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    /* Duplicate the given string using strdup */
+    new_node->str = strdup(str);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return (NULL);
+    }
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+    /* Update the new node's next pointer to the current head */
+    new_node->next = *head;
 
-	return (new_node);
+    /* Update the head to point to the new node */
+    *head = new_node;
+
+    return (new_node);
 }
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,17 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
 /* Structure for the linked list */
-typedef struct list_s
-{
+typedef struct list_s {
     char *str;
-    int len;
     struct list_s *next;
 } list_t;
 
-/* Function prototype for add_node */
+/* Function to add a new node at the beginning of the list */
 list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
