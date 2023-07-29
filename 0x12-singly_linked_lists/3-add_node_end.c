bf1db5d07cb2e520e8c3bbb668d4d462756254d4
Cetley7 <cheboiwendy@gmail.com>
2023-07-29 14:03:21 -0700

    commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,11 +9,11 @@
  */
 int _str_len(const char *s)
 {
-    int length;
+	int length;
 
-    for (length = 0; s[length]; length++)
-        ;
-    return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
 }
 
 /**
@@ -24,41 +24,41 @@ int _str_len(const char *s)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    int string_length;
-    char *str_dup;
-    list_t *new_node, *temp;
+	int string_length;
+	char *str_dup;
+	list_t *new_node, *temp;
 
-    if (str == NULL || head == NULL)
-        return (NULL);
+	if (str == NULL || head == NULL)
+		return (NULL);
 
-    string_length = _str_len(str);
-    str_dup = strdup(str);
-    if (str_dup == NULL)
-        return (NULL);
+	string_length = _str_len(str);
+	str_dup = strdup(str);
+	if (str_dup == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        free(str_dup);
-        return (NULL);
-    }
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(str_dup);
+		return (NULL);
+	}
 
-    new_node->str = str_dup;
-    new_node->len = string_length;
-    new_node->next = NULL;
+	new_node->str = str_dup;
+	new_node->len = string_length;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
