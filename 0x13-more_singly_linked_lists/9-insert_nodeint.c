7da003a77a56bf2cf8078cbd117713c78ea212c6
Cetley7 <cheboiwendy@gmail.com>
2023-08-01 09:11:01 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h> /* For malloc */
+
+/**
+ * insert_nodeint_at_index - Inserts a new node in the list.
+ * @head: Double pointer to the head of the list.
+ * @idx: Index where the new node should be added (starts at 0).
+ * @n: Value to be stored in the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	if (head == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_nodeint(head, n));
+
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	listint_t *current = *head;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == idx - 1)
+		{
+			new_node->n = n;
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+
+		current = current->next;
+		count++;
+	}
+
+	free(new_node); /* Free the allocated memory if index is not found */
+	return (NULL);
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -43,5 +43,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /* Function to calculate the sum of all data (n) in the list */
 int sum_listint(listint_t *head);
 
+/* Function to insert a new node at a given position */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 #endif /* LISTS_H */
 
