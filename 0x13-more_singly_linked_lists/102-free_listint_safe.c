0667e57aff5e586cffb2784c51c581200853cf98
Cetley7 <cheboiwendy@gmail.com>
2023-08-01 12:11:30 -0700

    commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,29 +1,56 @@
 #include "lists.h"
-#include <stdlib.h> /* For free */
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: Double pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t list even if it contains a loop.
+ * @h: Pointer to a pointer to the head of the list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current = *h;
-	listint_t *temp;
+	listint_t *slow = *h;
+	listint_t *fast = *h;
 	size_t size = 0;
 
-	while (current != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		size++;
-		temp = current;
-		current = current->next;
-		free(temp);
-		if (temp <= current)
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast) /* Loop detected */
 		{
-			*h = NULL;
+			/* Move slow to the head, keeping fast at the meeting point */
+			slow = *h;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			/* Move both pointers until they meet again at the loop start */
+			while (fast->next != slow)
+			{
+				fast = fast->next;
+				size++;
+			}
+			size++;
+			fast->next = NULL;
 			break;
 		}
+	}
+
+	/* Free the rest of the list (if no loop or after removing the loop) */
+	while (*h != NULL)
+	{
+		listint_t *temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		size++;
 	}
 
 	return (size);
