7f9b37dc696ad09ef94d17da66336f54c6bd1d6f
Cetley7 <cheboiwendy@gmail.com>
2023-08-01 12:29:29 -0700

    commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,82 +1,37 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
- * loop_detected - Check if a loop is detected in a linked list.
- * @head: Pointer to the head of the list.
- *
- * Return: The address of the node where the loop starts, or NULL.
- */
-listint_t *loop_detected(listint_t *head)
-{
-	listint_t *tortoise = head;
-	listint_t *hare = head;
-
-	while (hare != NULL && hare->next != NULL)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			return (hare);
-		}
-	}
-
-	return (NULL);
-}
-
-/**
- * remove_loop - Remove the loop from a linked list.
- * @head: Pointer to the head of the list.
- * @loop_start: Pointer to the node where the loop starts.
- */
-void remove_loop(listint_t *head, listint_t *loop_start)
-{
-	listint_t *ptr1 = head;
-	listint_t *ptr2 = loop_start;
-
-	while (ptr1->next != ptr2->next)
-	{
-		ptr1 = ptr1->next;
-		ptr2 = ptr2->next;
-	}
-
-	ptr2->next = NULL;
-}
-
-/**
- * free_listint_safe - Frees a listint_t list even if it contains a loop.
- * @h: Pointer to a pointer to the head of the list.
- *
+ * free_listint_safe - Frees a listint_t list, even if it contains a loop.
+ * @h: Double pointer to the head of the list.
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *loop_start = loop_detected(*h);
 	size_t size = 0;
+	listint_t *current, *temp;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	if (loop_start != NULL)
-		remove_loop(*h, loop_start);
+	current = *h;
 
-	while (*h != NULL)
+	/* Traverse the list and free each node */
+	while (current != NULL)
 	{
-		listint_t *temp = *h;
-		*h = (*h)->next;
-		free(temp);
 		size++;
+		temp = current;
+		current = current->next;
+		free(temp);
+
+		/* Break the loop if we reach a node we have already visited */
+		if (current == *h)
+		{
+			*h = NULL; /* Set the head to NULL to avoid using a dangling pointer */
+			break;
+		}
 	}
 
 	return (size);
