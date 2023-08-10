ac6ba8e299b4932fc0598d081f72c59e827af840
Cetley7 <cheboiwendy@gmail.com>
2023-08-10 10:31:40 -0700

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
