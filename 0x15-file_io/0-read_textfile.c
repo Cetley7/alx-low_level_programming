656b36134c38fe9993086b510433c362c431826a
Cetley7 <Cetley7>
2023-09-05 05:15:31 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,53 @@
 #include "main.h"
 #include <fcntl.h>
-
-#define BUF_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - Read and print a text file.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- * Return: The actual number of letters read and printed, or 0 on failure.
+ *
+ * Return: The actual number of letters read and printed, 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUF_SIZE];
+    int file_descriptor;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+        return (0);
 
-	bytes_read = read(fd, buffer, BUF_SIZE);
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(file_descriptor);
+        return (0);
+    }
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		return (0);
-	}
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1)
+    {
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
 
-	close(fd);
-	return (bytes_written);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
+
+    close(file_descriptor);
+    free(buffer);
+    return (bytes_written);
 }
 
