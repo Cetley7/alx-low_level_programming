f2e11967aa703e308e4e064fa4b2eaca661f7d3b
Cetley7 <Cetley7>
2023-09-05 05:19:11 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output.
@@ -12,42 +10,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        close(file_descriptor);
-        free(buffer);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(file_descriptor);
-        free(buffer);
-        return (0);
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
 
-    close(file_descriptor);
-    free(buffer);
-    return (bytes_written);
+	close(file_descriptor);
+	free(buffer);
+	return (bytes_written);
 }
 
