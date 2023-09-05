bcf1877976a6ddb59ff2090c7595de4571ff921e
Cetley7 <Cetley7>
2023-09-05 12:14:23 -0700

    commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - Gets the value of a bit at a given index
+ * @n: The unsigned long integer
+ * @index: The index of the bit to retrieve (0-based)
+ *
+ * Return: The value of the bit at the given index or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1); /* Index out of bounds */
+
+	return ((n >> index) & 1);
+}
+
