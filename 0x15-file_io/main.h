7ddcd55495aef26da719513ad7110a12f4159cc6
Cetley7 <cheboiwendy@gmail.com>
2023-10-05 07:19:34 -0700

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
+#ifndef FILE_IO
+#define FILE_IO
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#ifndef FILE_IO
-#define FILE_IO
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
